288f64ca3359d1a3506ad6ae8198fd277b11eb5a
lindsayflores0928 <141559631+lindsayflores0928@users.noreply.github.com>
2024-02-28 12:25:58 -0500

    update

--- js/03-feedback.js
+++ js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from 'lodash.throttle';
+
+const feedbackForm = document.querySelector(".feedback-form");
+const email = document.querySelector("input");
+const message = document.querySelector("textarea");
+
+const localSetter = () => {
+    const fd = new FormData(feedbackForm);
+    const obj = Object.fromEntries(fd);
+
+    localStorage.setItem("feedback-form-state", JSON.stringify(obj));
+}
+
+const formData = (e) => {
+    e.preventDefault();
+    const { email, message} = e.target;
+    const fd = new FormData(feedbackForm);
+    const obj = Object.fromEntries(fd);
+
+    if (email.value !== "" && message.value !== "") {
+        localStorage.removeItem("feedback-form-state");
+        feedbackForm.reset();
+        console.log(obj);
+    } else {
+        alert("Fill up the fields first!");
+    }
+    
+}
+
+feedbackForm.addEventListener("input", throttle(localSetter, 500));
+feedbackForm.addEventListener("submit", formData);
+
+let localData = JSON.parse(localStorage.getItem("feedback-form-state"));
+
+if (localData !== null) {
+    email.value = localData.email;
+    message.value = localData.message;
+} 
 
